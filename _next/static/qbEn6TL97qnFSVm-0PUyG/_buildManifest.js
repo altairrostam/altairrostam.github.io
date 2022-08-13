9edf12f07ac6bb0808ee5c12f93dc9bac156ac1a
altairrostam <110603940+altairrostam@users.noreply.github.com>
2022-08-13 11:00:22 +0000

    Deploying to gh-pages from @ altairrostam/altairrostam.github.io@d657e3e5642267d91567158077429d96082c11c3 🚀

---
+++ _next/static/qbEn6TL97qnFSVm-0PUyG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a5043688cd99bfb2.css","static/chunks/pages/index-47a4cf37ac850e4f.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
